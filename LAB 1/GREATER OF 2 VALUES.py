9d8f3e0b0c1751f952c0ab35a2fe4deeae84c8f7
ashithsanjaykumar <ashithsanjaykumar6@gmail.com>
2026-02-25 14:18:42 +0530

    Add files via upload

---
+++ LAB 1/GREATER OF 2 VALUES.py
@@ -0,0 +1,8 @@
+a = int(input("enter a value : "))
+b = int(input("enter a value : "))
+
+if (a > b):
+    print(a, "is greater than", b)
+
+else:
+    print(b, "is greater than", a)
---
+++ LAB2/sumof2digitsofnumber.py
@@ -0,0 +1,4 @@
+value = int(input("Enter a 2 digit number :"))
+a = value % 10
+b = int(value / 10)
+print("Sum of the digits of the number :", a+b)
